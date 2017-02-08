9db47ab8dfb0fae0cf643848d9bbf09548000b25
chet00175 <chet00175@yahoo.com>
2017-02-08 21:51:33 +1300

    Added the initial setup of chess pieces on the board

--- js/board_functionality.js
+++ js/board_functionality.js
@@ -1,3 +1,21 @@
+// All possible chess pieces.
+var CHESS_PIECES = { WHITE_PAWN : "WHITE_PAWN", WHITE_BISHOP : "WHITE_BISHOP", WHITE_KNIGHT : "WHITE_KNIGHT", WHITE_ROOK : "WHITE_ROOK", WHITE_KING : "WHITE_KING", WHITE_QUEEN : "WHITE_QUEEN",
+	BLACK_PAWN : "BLACK_PAWN", BLACK_BISHOP : "BLACK_BISHOP", BLACK_KNIGHT : "BLACK_KNIGHT", BLACK_ROOK : "BLACK_ROOK", BLACK_KING : "BLACK_KING", BLACK_QUEEN : "BLACK_QUEEN" };
+
+
+// Declaring global variables to keep track of the position of the kings on the board.
+var whiteKingPos;
+var blackKingPos;
+
+
+
+
+// Representing position on the board.
+function BoardPosition(x, y) {
+	this.x = x;
+	this.y = y;
+}
+
 function createBoard() {
 	var table = $("<table id='' cellspacing='0' cellpadding='0'></table>"); // might need cellspacing and cellpadding set to 0
 
@@ -20,7 +38,9 @@ function createBoard() {
 			$(cell).css('backgroundColor',colour);
 			$(cell).css('width',100);
 			$(cell).css('height',100);
+			$(cell).css('textAlign', 'center'); // Ensure chess pieces are on the centre of the square.
 			$(cell).data('piece', 'empty'); // indicates that the cell currently has no chess piece in it.
+			$(cell).data('position', new BoardPosition(j,i));
 			
 			$(row).append(cell);
 			// Switch between square colours on the board.
@@ -44,10 +64,160 @@ function createBoard() {
 	return table;
 }
 
+// Initial placement of chess pieces on the board.
+function placePieces() {
+	for (var i = 1; i <= 8; i++) {
+		$('#2'+i).data('piece',CHESS_PIECES.WHITE_PAWN);
+	}
+
+	for (var i = 1; i <= 8; i++) {
+		$('#7'+i).data('piece',CHESS_PIECES.BLACK_PAWN);
+	}
+
+	// White capital (non-pawn) pieces
+	$('#18').data('piece',CHESS_PIECES.WHITE_ROOK);
+	$('#11').data('piece',CHESS_PIECES.WHITE_ROOK);
+	$('#17').data('piece',CHESS_PIECES.WHITE_KNIGHT);
+	$('#12').data('piece',CHESS_PIECES.WHITE_KNIGHT);
+	$('#13').data('piece',CHESS_PIECES.WHITE_BISHOP);
+	$('#16').data('piece',CHESS_PIECES.WHITE_BISHOP);
+	$('#15').data('piece',CHESS_PIECES.WHITE_KING);
+	$('#14').data('piece',CHESS_PIECES.WHITE_QUEEN);
+
+	// Black capital (non-pawn) pieces
+	$('#88').data('piece',CHESS_PIECES.BLACK_ROOK);
+	$('#81').data('piece',CHESS_PIECES.BLACK_ROOK);
+	$('#87').data('piece',CHESS_PIECES.BLACK_KNIGHT);
+	$('#82').data('piece',CHESS_PIECES.BLACK_KNIGHT);
+	$('#83').data('piece',CHESS_PIECES.BLACK_BISHOP);
+	$('#86').data('piece',CHESS_PIECES.BLACK_BISHOP);
+	$('#85').data('piece',CHESS_PIECES.BLACK_KING);
+	$('#84').data('piece',CHESS_PIECES.BLACK_QUEEN);
+
+	whiteKingPos = $('#15').data('position');
+	blackKingPos = $('#85').data('position');
+
+	// Draw pieces onto board
+
+	// Piece size:
+	var pieceWidth = 50;
+	var pieceHeight = 50;
+
+	for (var i = 1; i <= 8; i++) {
+		var img = $('<img>');
+		img.attr('src', 'images/WHITE_PAWN.ico');
+		$(img).width(pieceWidth);
+		$(img).height(pieceHeight);
+		img.appendTo('#2'+i);
+	}
+
+	for (var i = 1; i <= 8; i++) {
+		var img = $('<img>');
+		img.attr('src', 'images/BLACK_PAWN.ico');
+		$(img).width(pieceWidth);
+		$(img).height(pieceHeight);
+		img.appendTo('#7'+i);
+	}
+
+	// White capital (non-pawn) pieces
+	var img = $('<img>');
+	img.attr('src', 'images/WHITE_ROOK.ico');
+	$(img).width(pieceWidth);
+	$(img).height(pieceHeight);
+	img.appendTo('#18');
+	img = $('<img>');
+	img.attr('src', 'images/WHITE_ROOK.ico');
+	$(img).width(pieceWidth);
+	$(img).height(pieceHeight);
+	img.appendTo('#11');
+
+	img = $('<img>');
+	img.attr('src', 'images/WHITE_KNIGHT.ico');
+	$(img).width(pieceWidth);
+	$(img).height(pieceHeight);
+	img.appendTo('#17');
+	img = $('<img>');
+	img.attr('src', 'images/WHITE_KNIGHT.ico');
+	$(img).width(pieceWidth);
+	$(img).height(pieceHeight);
+	img.appendTo('#12');
+
+	img = $('<img>');
+	img.attr('src', 'images/WHITE_BISHOP.ico');
+	$(img).width(pieceWidth);
+	$(img).height(pieceHeight);
+	img.appendTo('#13');
+	img = $('<img>');
+	img.attr('src', 'images/WHITE_BISHOP.ico');
+	$(img).width(pieceWidth);
+	$(img).height(pieceHeight);
+	img.appendTo('#16');
+
+	img = $('<img>');
+	img.attr('src', 'images/WHITE_KING.ico');
+	$(img).width(pieceWidth);
+	$(img).height(pieceHeight);
+	img.appendTo('#15');
+
+	img = $('<img>');
+	img.attr('src', 'images/WHITE_QUEEN.ico');
+	$(img).width(pieceWidth);
+	$(img).height(pieceHeight);
+	img.appendTo('#14');
+
+	// Black capital (non-pawn) pieces
+	img = $('<img>');
+	img.attr('src', 'images/BLACK_ROOK.ico');
+	$(img).width(pieceWidth);
+	$(img).height(pieceHeight);
+	img.appendTo('#88');
+	img = $('<img>');
+	img.attr('src', 'images/BLACK_ROOK.ico');
+	$(img).width(pieceWidth);
+	$(img).height(pieceHeight);
+	img.appendTo('#81');
+
+	img = $('<img>');
+	img.attr('src', 'images/BLACK_KNIGHT.ico');
+	$(img).width(pieceWidth);
+	$(img).height(pieceHeight);
+	img.appendTo('#87');
+	img = $('<img>');
+	img.attr('src', 'images/BLACK_KNIGHT.ico');
+	$(img).width(pieceWidth);
+	$(img).height(pieceHeight);
+	img.appendTo('#82');
+
+	img = $('<img>');
+	img.attr('src', 'images/BLACK_BISHOP.ico');
+	$(img).width(pieceWidth);
+	$(img).height(pieceHeight);
+	img.appendTo('#83');
+	img = $('<img>');
+	img.attr('src', 'images/BLACK_BISHOP.ico');
+	$(img).width(pieceWidth);
+	$(img).height(pieceHeight);
+	img.appendTo('#86');
+
+	img = $('<img>');
+	img.attr('src', 'images/BLACK_KING.ico');
+	$(img).width(pieceWidth);
+	$(img).height(pieceHeight);
+	img.appendTo('#85');
+
+	img = $('<img>');
+	img.attr('src', 'images/BLACK_QUEEN.ico');
+	$(img).width(pieceWidth);
+	$(img).height(pieceHeight);
+	img.appendTo('#84');
+
+}
+
 
 function initGame() {	
 	// create chess board
 	var table = createBoard();
 	$('#board').append(table);
-	
+
+	placePieces();
 }

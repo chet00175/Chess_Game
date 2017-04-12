35dbc9edc78daea58d085d3b608001ea036a2860
chet00175 <chet00175@yahoo.com>
2017-04-12 21:24:42 +1200

    Updated readme file to include instructions for running the application using vagrant

--- _vagrant/scripts.sh
+++ _vagrant/scripts.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
 sudo apt-get update
+
+# Installing apache
 sudo apt-get -y install apache2
 
+# Installing and configuring mysql
 export DEBIAN_FRONTEND="noninteractive"
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password mysql"
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password mysql"
 sudo apt-get -y install mysql-server libapache2-mod-auth-mysql
 mysql -sfu root < "mysql_secure_install.sql"
 
+# Install PHP5 and configuring with MySQL
 sudo apt-get -y install php5 libapache2-mod-php5 php5-mysql
 
+# Restarting apache and mysql
 sudo /etc/init.d/apache2 restart
 sudo /etc/init.d/mysql restart
 

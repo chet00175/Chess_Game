372d74d3c9cca3930cf4460ddc60cd84dd6a1745
chet00175 <chet00175@yahoo.com>
2017-04-12 20:46:47 +1200

    Adding files for enabling automation of lamp environment using vagrant

---
+++ _vagrant/scripts.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get -y install apache2
+
+export DEBIAN_FRONTEND="noninteractive"
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password mysql"
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password mysql"
+sudo apt-get -y install mysql-server libapache2-mod-auth-mysql
+mysql -sfu root < "mysql_secure_install.sql"
+
+sudo apt-get -y install php5 libapache2-mod-php5 php5-mysql
+
+sudo /etc/init.d/apache2 restart
+sudo /etc/init.d/mysql restart
+
+exit 0
